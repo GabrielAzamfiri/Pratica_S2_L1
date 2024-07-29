a51d1d03783d4e41e850e8e78b7219c7d9864c93
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-07-29 14:56:54 +0200

    add Es 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int[] numeri = new int[5];
+        for (int i = 0; i < numeri.length; i++) {
+            try {
+                System.out.println("Inserisci un numero da 1 a 10");
+                int numUtente = Integer.parseInt(scanner.nextLine());
+                if (numUtente < 1 || numUtente > 10) {
+                    System.err.println("Il numero non è compreso tra 1 e 10. Riprova");
+                    i--;
+                } else {
+                    numeri[i] = numUtente;
+                    System.out.println(Arrays.toString(numeri));
+
+                }
+                if (numUtente == 0) break;
+                if (i == 4) i = -1;
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+                System.err.println("Il valore non è compreso tra 1 e 10. Riprova");
+                i--;
+            }
+
+
+        }
+
+
     }
 }
